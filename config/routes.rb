9e1258763c3d3413cc26ac6f1f51f6f227e5d764
okahontas0321 <okahontas0321@ezweb.ne.jp>
2019-10-02 23:14:03 +0900

    edit_profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
-
+  get 'users/profile'
   root 'tops#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
