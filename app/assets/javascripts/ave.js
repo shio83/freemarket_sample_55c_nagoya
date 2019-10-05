60e0f4281361110d2cca7433a85edba597223609
shinji-2017 <shinji2017@icloud.com>
2019-10-05 21:46:02 +0900

    スライドショー！！！！！！！

---
+++ app/assets/javascripts/ave.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".slider").slick({
+    autoplay: true,
+    autoplayspeed: 1000,
+    pauseOnFocus: false,
+    dots: true,
+    prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+    nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+  });
+});
+ 
