d5689520ce4b74653980cc217229ba78b46a37e4
okahontas0321 <okahontas0321@ezweb.ne.jp>
2019-10-11 20:24:37 +0900

    ぼかし

---
+++ app/assets/javascripts/details.js
@@ -0,0 +1,18 @@
+$(function(){
+  $(".slider-for").slick({
+    slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+});
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
 class ProductsController < ApplicationController
-  def 
+  def details
+  end
 end
