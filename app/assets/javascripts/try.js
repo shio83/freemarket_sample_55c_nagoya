922cc2e996cd866282a067d68d4c911e86202e5e
kirakira33 <kirakiraohoshisam41@gmail.com>
2019-11-02 16:00:40 +0900

    view
    ビューを追加

---
+++ app/assets/javascripts/try.js
@@ -0,0 +1,15 @@
+// //現在の年数オブジェクトを4桁で生成
+// var time = new Date();
+// var year = time.getFullYear();
+// //1900年まで表示
+// for (var i = year; i >= 1900; i--) {
+//     $('#year').append('<option value="' + i + '">' + i + '</option>');
+// }
+// //1～12の数字を生成
+// for (var i = 1; i <= 12; i++) {
+//     $('#month').append('<option value="' + i + '">' + i + '</option>');
+// }
+// //1～31の数字を生成
+// for (var i = 1; i <= 31; i++) {
+//     $('#day').append('<option value="' + i + '">' + i + '</option>');
+// }
