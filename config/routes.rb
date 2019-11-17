7a9a926c2271e2a7ba7a4e59b6308b91f0001fd1
kirakira33 <kirakiraohoshisam41@gmail.com>
2019-11-17 15:44:34 +0900

    add_view(login)
    ログインページ追加
    不要なビュー、コントローラー内の不要アクションの削除

--- config/routes.rb
+++ config/routes.rb
@@ -39,17 +39,6 @@ Rails.application.routes.draw do
       get 'detail_done'  #登録完了後
     end
   end
-  # 本人情報の新規登録
-  get 'users/registration' => "users#registration"
-  post 'users/registration' => "users#create" ,as: 'users_create'
-  # 本人情報の新規登録_メールアドレス_tel
-  get 'users/detail_tel' => "users#detail_tel"
-  # 本人情報の新規登録_メールアドレス_zip
-  get 'users/detail_zip' => "users#detail_zip"
-  # 本人情報の新規登録_メールアドレス_payment
-  get 'users/detail_payment' => "users#detail_payment"
-  # 本人情報の新規登録_メールアドレス_done
-  get 'users/detail_done' => "users#detail_done"
   
   root 'tops#index'
   # post "users/:parent_id/create" => "users#create"
