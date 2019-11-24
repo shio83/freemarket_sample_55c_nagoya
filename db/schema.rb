826b2f32b7d36d13a545e69398ad823f4de8fa9b
shinji-2017 <shinji2017@icloud.com>
2019-11-24 19:47:50 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,36 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20191122124352) do
 
-  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "last_name",             null: false
-    t.string   "first_name",            null: false
-    t.string   "last_name_kana",        null: false
-    t.string   "first_name_kana",       null: false
-    t.string   "birth_year",            null: false
-    t.string   "birth_month",           null: false
-    t.string   "birth_day",             null: false
-    t.integer  "user_id"
-    t.string   "phone_number",          null: false
-    t.string   "zipcode",               null: false
-    t.string   "prefecture",            null: false
-    t.string   "city",                  null: false
-    t.string   "address",               null: false
-    t.string   "building",              null: false
-    t.string   "authorization_token",   null: false
-    t.string   "security_code",         null: false
-    t.string   "expiration_date_month", null: false
-    t.string   "expiration_date_year",  null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
-  end
-
-  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -93,14 +63,13 @@ ActiveRecord::Schema.define(version: 20191122124352) do
     t.integer  "shipping_fee",                  default: 0, null: false
     t.integer  "shipping_region",               default: 0, null: false
     t.integer  "shipping_date",                 default: 0, null: false
-    t.integer  "size",                          default: 0, null: false
+    t.string   "size",                                      null: false
     t.string   "price",                                     null: false
+    t.string   "brand"
     t.integer  "user_id"
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.integer  "category_id"
-    t.integer  "brand_id"
-    t.index ["brand_id"], name: "index_products_on_brand_id", using: :btree
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
@@ -156,6 +125,23 @@ ActiveRecord::Schema.define(version: 20191122124352) do
     t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "last_name",                           null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name_kana",                      null: false
+    t.string   "first_name_kana",                     null: false
+    t.string   "birth_year",                          null: false
+    t.string   "birth_month",                         null: false
+    t.string   "birth_day",                           null: false
+    t.string   "phone_number",                        null: false
+    t.string   "zipcode",                             null: false
+    t.integer  "prefecture",             default: 0,  null: false
+    t.string   "city",                                null: false
+    t.string   "address",                             null: false
+    t.string   "building",                            null: false
+    t.string   "authorization_token",                 null: false
+    t.string   "security_code",                       null: false
+    t.string   "expiration_date_month",               null: false
+    t.string   "expiration_date_year",                null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -165,7 +151,6 @@ ActiveRecord::Schema.define(version: 20191122124352) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "addresses", "users"
   add_foreign_key "category_sizes", "categories"
   add_foreign_key "category_sizes", "sizes"
   add_foreign_key "comments", "products"
@@ -173,7 +158,6 @@ ActiveRecord::Schema.define(version: 20191122124352) do
   add_foreign_key "images", "products"
   add_foreign_key "likes", "products"
   add_foreign_key "likes", "users"
-  add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
   add_foreign_key "profiles", "users"
